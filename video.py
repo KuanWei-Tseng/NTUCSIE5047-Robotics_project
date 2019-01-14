00ac6acd3b908882a291013f294e7dc7500aef8e
steven0402 <b05902091@ntu.edu.tw>
2019-01-14 09:14:52 +0800

    recording

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera
 
 with picamera.PiCamera() as record:
-	record.resolution = ()
+	record.resolution = (1280, 480)
 	record.start_recording('demo_video.h264')
 	record.wait_recording(30)
 	record.stop_recording()
